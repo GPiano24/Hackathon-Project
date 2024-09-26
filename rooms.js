6f234f1992a99af27ffbfa8b91a27c0a78bf632f
ErnestLontoc <115440511+ErnestLontoc@users.noreply.github.com>
2024-09-26 19:55:41 +0800

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -4,31 +4,31 @@ module.exports = {
             id: 1,
             name: "Room 1",
             capacity: 10,
-            status: "Available"
+            
         },
         {
             id: 2,
             name: "Room 2",
             capacity: 20,
-            status: "Available"
+            
         },
         {
             id: 3,
             name: "Room 3",
             capacity: 30,
-            status: "Available"
+            
         },
         {
             id: 4,
             name: "Room 4",
             capacity: 40,
-            status: "Not Available"
+            
         },
         {
             id: 5,
             name: "Room 5",
             capacity: 50,
-            status: "Available"
+            
         }
     ]
 }
