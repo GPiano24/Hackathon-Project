f478ca7ea6ab6a510b2795d260f454d0389cb946
NicoleMagallanes <nicolemagallanes026@gmail.com>
2024-09-26 14:14:05 +0800

    Update dependencies and add new pages
    - Update react-router and react-router-dom to version 6.26.2
    - Add new pages: AuthPage, BookingPage, ContactUsPage, and AdminBookingPage

--- hackaton-app/src/App.js
+++ hackaton-app/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import ContactUsPage from "./pages/ContactUsPage";
+import AdminBookingPage from "./pages/AdminBookingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/contact" element={<ContactUsPage />} />
+            <Route path="/a_booking" element={<AdminBookingPage />} />
+            <Route path="/booking" element={<AdminBookingPage />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ hackaton-app/src/pages/AdminBookingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminBookingPage = () => {
+  return (
+    <div>
+      <h1>Admin Booking Page</h1>
+    </div>
+  );
+};
+
+export default AdminBookingPage;
