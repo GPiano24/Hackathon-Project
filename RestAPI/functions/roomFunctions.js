90827281ebbaa222f90cedabfca7ad9453625dfb
MITDCVhea <vicenvh@MFCGD.com>
2024-09-26 20:26:14 +0800

    Refactored rooms routes for unit testing.

--- RestAPI/functions/roomFunctions.js
+++ RestAPI/functions/roomFunctions.js
@@ -1,5 +1,27 @@
 import connection  from '../database/dbConnection.js';
 
+export async function getRooms(capacity) {
+    const SELECT_QUERY = "select ROOM_ID, ROOM_NAME, CAPACITY from ROOMS"
+
+    let rooms = [];
+
+    return new Promise((resolve, reject) => {
+        connection.query(SELECT_QUERY, async (err, result) => {
+            if (err) throw err;
+            rooms =  await result;
+
+            if(capacity && rooms){
+                rooms = rooms.filter(room => room.capacity === parseInt(capacity));
+            }
+        
+            if(!rooms || rooms.length == 0) {
+                throw new Error("Error retrieving rooms!");
+            } 
+            resolve(rooms)
+        });
+    });
+}
+
 export async function getRoom(roomId) {
     const SELECT_QUERY = `select ROOM_ID, ROOM_NAME, CAPACITY from ROOMS where ROOM_ID="${roomId}"`
 
--- RestAPI/src/setupRoomRoutes.js
+++ RestAPI/src/setupRoomRoutes.js
@@ -1,5 +1,5 @@
 import connection  from '../database/dbConnection.js';
-import { getRoom }  from '../functions/roomFunctions.js';
+import { getRoom, getRooms }  from '../functions/roomFunctions.js';
 
 const API_URL = 'http://localhost:4000';
 
@@ -7,24 +7,10 @@ export function setupRoomRoutes (app) {
     
     app.get("/rooms", async (req, res) => {
         try {
-            const SELECT_QUERY = "select ROOM_ID, ROOM_NAME, CAPACITY from ROOMS"
             const capacity = req.query.capacity;
 
-            let rooms = [];
-
-            connection.query(SELECT_QUERY, async (err, result) => {
-                if (err) throw err;
-                rooms =  await result;
-
-                if(capacity && rooms){
-                    rooms = rooms.filter(room => room.capacity === parseInt(capacity));
-                }
-            
-                if(!rooms || rooms.length == 0) {
-                    throw new Error("Error retrieving rooms!");
-                } 
-                res.json(rooms)
-            });
+            let rooms = await getRooms(capacity);
+            res.json(rooms);
         } catch(error) {
             res.status(400).json({error: error.message});
         }
