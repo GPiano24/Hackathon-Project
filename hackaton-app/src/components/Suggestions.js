4bd6067e17666c5d25f2fc09c865d0e8677f3de2
Kess Entrata <kesselentrata@gmail.com>
2024-09-27 11:03:03 +0800

    only map if there is rooms available

--- hackaton-app/src/components/Suggestions.js
+++ hackaton-app/src/components/Suggestions.js
@@ -23,22 +23,23 @@ const Suggestions = () => {
   return (
     <div className="slider">
       <div className="slider-content">
-        {rooms.map((room, index) => (
-          <div
-            key={index}
-            className="card bg-white shadow-lg rounded-lg p-6 m-4 flex-shrink-0 transform transition-transform hover:scale-105"
-          >
-            <h3 className="text-xl font-semibold mb-2 text-gray-800">
-              {room.ROOM_NAME}
-            </h3>
-            <p className="text-gray-600">
-              <span className="font-medium">Room Number:</span> {room.ROOM_ID}
-            </p>
-            <p className="text-gray-600">
-              <span className="font-medium">Capacity:</span> {room.CAPACITY}
-            </p>
-          </div>
-        ))}
+        {rooms &&
+          rooms.map((room, index) => (
+            <div
+              key={index}
+              className="card bg-white shadow-lg rounded-lg p-6 m-4 flex-shrink-0 transform transition-transform hover:scale-105"
+            >
+              <h3 className="text-xl font-semibold mb-2 text-gray-800">
+                {room.ROOM_NAME}
+              </h3>
+              <p className="text-gray-600">
+                <span className="font-medium">Room Number:</span> {room.ROOM_ID}
+              </p>
+              <p className="text-gray-600">
+                <span className="font-medium">Capacity:</span> {room.CAPACITY}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );
