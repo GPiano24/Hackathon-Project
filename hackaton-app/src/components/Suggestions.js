731ed199a6af40a47c02736eeaab458859b7fbc4
Kess Entrata <kesselentrata@gmail.com>
2024-09-27 10:16:13 +0800

    initial commits

--- hackaton-app/src/components/Suggestions.js
+++ hackaton-app/src/components/Suggestions.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
 import "../styles/suggestions.css";
+
+const API_URL = "http://localhost:4000/rooms";
+
+const GET_ROOMS = gql`
+  query GetRooms {
+    rooms {
+      ROOM_ID
+      ROOM_NAME
+      CAPACITY
+    }
+  }
+`;
 
 const suggestions = [
   {
@@ -23,10 +36,28 @@ const suggestions = [
 ];
 
 const Suggestions = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setRooms(data);
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+      }
+    };
+
+    fetchRooms();
+  }, []);
+
+  console.log("rooms:", rooms);
+
   return (
     <div className="slider">
       <div className="slider-content">
-        {suggestions.map((suggestion, index) => (
+        {rooms.map((suggestion, index) => (
           <div
             key={index}
             className="card bg-white shadow-lg rounded-lg p-6 m-4 flex-shrink-0 transform transition-transform hover:scale-105"
@@ -36,27 +67,7 @@ const Suggestions = () => {
             </h3>
             <p className="text-gray-600">
               <span className="font-medium">Room Number:</span>{" "}
-              {suggestion.roomNumber}
-            </p>
-            <p className="text-gray-600">
-              <span className="font-medium">Floor:</span> {suggestion.floor}
-            </p>
-            <p className="text-gray-600">
-              <span className="font-medium">Time:</span> {suggestion.time}
-            </p>
-          </div>
-        ))}
-        {suggestions.map((suggestion, index) => (
-          <div
-            key={index + suggestions.length}
-            className="card bg-white shadow-lg rounded-lg p-6 m-4 flex-shrink-0 transform transition-transform hover:scale-105"
-          >
-            <h3 className="text-xl font-semibold mb-2 text-gray-800">
-              {suggestion.room}
-            </h3>
-            <p className="text-gray-600">
-              <span className="font-medium">Room Number:</span>{" "}
-              {suggestion.roomNumber}
+              {suggestion.CAPACITY}
             </p>
             <p className="text-gray-600">
               <span className="font-medium">Floor:</span> {suggestion.floor}
